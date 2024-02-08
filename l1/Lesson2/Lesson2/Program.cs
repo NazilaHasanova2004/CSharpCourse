d8cf7fb52c83380d4bb68ef53e1ae62461d38fb9
Nazila Hasanova <159264619+NazilaHasanova2004@users.noreply.github.com>
2024-02-08 18:25:21 +0400

    hey

---
+++ l1/Lesson2/Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            float rad = float.Parse(Console.ReadLine());
+            Console.WriteLine("Perimeter:"+2*Math.PI*rad);
+            Console.WriteLine("Area:" +  Math.PI * rad*rad);
+
+            Console.ReadLine();
+        }
+    }
+}
